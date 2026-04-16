f8d856099e6e9b0f896483c546d6c9790bab0582
tsyamagu <tsyamagu@C010423674.zipangu.cac.co.jp>
2026-04-16 13:29:31 +0900

    Q3完了

--- java_comprehension_exercises_volume1/src/basic/question03/Question03_1.java
+++ java_comprehension_exercises_volume1/src/basic/question03/Question03_1.java
@@ -9,6 +9,12 @@ public class Question03_1 {
 	public static void main(String[] args) {
 
 		// TODO:ここに実装
-		
+
+		int price = 30;
+		int totalBudget = 300;
+		int quantity = totalBudget / price;
+
+		System.out.println("商品の購入数は[" + quantity + "の値]個です。");
+
 	}
 }
